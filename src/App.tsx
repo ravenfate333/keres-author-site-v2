85a1414cace794988bb8eebd72afaebf8c68bcde
skeleton <ravenfate333@gmail.com>
2025-07-07 18:53:53 -0600

    Initial project setup with Vite, TS, and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900">
+      <h1 className="text-5xl font-bold text-white underline decoration-sky-500">
+        Hello, Tailwind!
+      </h1>
+    </div>
+  )
+}
+export default App
+
